d7cc79e5fcbd0947646e1fff546a435c6559084d
Brian Harrington <brharrington@gmail.com>
2015-02-04 12:51:27 -0800

    setup with nebula daemon plugin

---
+++ atlas/root/var/lib/atlas/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source /command/service_functions.sh
+
+typeset -x `stat --printf "userowner=%U\ngroupowner=%G\n" $0`
+
+logDir=/var/log/atlas
+mkdir -p $logDir && chown $userowner:$groupowner $logDir && chmod 755 $logDir
+
+binDir=$(cd $(dirname $0) && pwd)
+source /usr/share/tomcat7/bin/setenv.sh
+exec /usr/local/bin/setuidgid $userowner $JAVA_HOME/bin/java $JAVA_OPTS \
+  -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager \
+  -Dlog4j.configurationFile=$binDir/log4j.xml \
+  -jar "$binDir/main.jar"
